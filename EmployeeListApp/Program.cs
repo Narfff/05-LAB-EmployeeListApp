cf967d6d9c299f7c5481e32a9683f1edf536aba5
Fran Habijanic <fran.habijanic@gmail.com>
2025-12-18 19:52:44 +0100

    cetvrtina

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -2,6 +2,8 @@ using EmployeeListApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using EmployeeListApp.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<EmployeesService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
--- EmployeeListApp/Services/EmployeesService.cs
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -1,6 +1,54 @@
-﻿namespace EmployeeListApp.Services
+﻿using EmployeeListApp.Data;
+
+namespace EmployeeListApp.Services
 {
 	public class EmployeesService
 	{
+		AppDbContext _context;
+		public EmployeesService(AppDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<List<Employee>> GetEmployeesAsync()
+		{
+			var result = _context.Employees;
+			return await Task.FromResult(result.ToList());
+		}
+		public async Task<Employee> GetEmployeeByIdAsync(string id)
+		{
+			return await _context.Employees.FindAsync(id);
+		}
+		public async Task<Employee> InsertEmployeeAsync(Employee Employee)
+		{
+			_context.Employees.Add(Employee);
+			await _context.SaveChangesAsync();
+			return Employee;
+		}
+		public async Task<Employee> UpdateEmployeeAsync(string id, Employee s)
+		{
+			var Employee = await _context.Employees.FindAsync(id);
+			if (Employee == null)
+				return null;
+			Employee.Id = s.Id;
+			Employee.FullName = s.FullName;
+			Employee.Department = s.Department;
+			Employee.Salary = s.Salary;
+			_context.Employees.Update(Employee);
+			await _context.SaveChangesAsync();
+			return Employee;
+		}
+		public async Task<Employee> DeleteEmployeeAsync(string id)
+		{
+			var Employee = await _context.Employees.FindAsync(id);
+			if (Employee == null)
+				return null;
+			_context.Employees.Remove(Employee);
+			await _context.SaveChangesAsync();
+			return Employee;
+		}
+		private bool EmployeeExists(string id)
+		{
+			return _context.Employees.Any(e => e.Id == id);
+		}
 	}
 }
